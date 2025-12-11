799ac78043b5a8e206bab63c0677b5106c38fbbe
Henry M. Leme <henrymleme1000@gmail.com>
2025-12-11 05:16:26 -0300

    concerto codigo de primeira entrada no site css

--- src/script/theme.js
+++ src/script/theme.js
@@ -1,8 +1,12 @@
-//CARREGAR TEMA
 const styleCSS = document.querySelector("#tema-css");
 const unitema = localStorage.getItem('tema');
-if (unitema !== styleCSS.href) {
-    styleCSS.href = unitema;
-} else if (unitema == null) {
-    localStorage.setItem('tema', "../styles/switch-dark.css");
+const temaPadrao = "../styles/switch-dark.css";
+
+if (unitema === null) {
+    localStorage.setItem('tema', temaPadrao);
+    styleCSS.setAttribute("href", temaPadrao);
+
+} else {if (styleCSS.getAttribute("href") !== unitema) {
+        styleCSS.setAttribute("href", unitema);
+    }
 }
